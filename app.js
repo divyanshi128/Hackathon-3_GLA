b20b49de0a6016aa9ab7a79110bac1c4e457a70f
Divyanshi Agrawal <92880987+divyanshi128@users.noreply.github.com>
2023-07-29 12:12:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const path=require('path');
 const seedDB = require('./seed');
 const quoteRoute= require('./routes/quoteRoute')
 const methodOverride = require('method-override')
+const DB_URL= mongodb://127.0.0.1:27017/hackathon_3
 
-mongoose.connect('mongodb://127.0.0.1:27017/hackathon_3')
+mongoose.connect(DB_URL)
 .then(()=>{console.log('DB connected')})
 .catch((err)=>{console.log(err)})
 
